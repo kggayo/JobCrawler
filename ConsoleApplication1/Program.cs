69e3d11f985b76b446174f5c5f74f3a9d6994ac6
kgayo <kgayo@SSAI-0161-HP>
2017-02-01 00:18:43 +0800

    Add project files.

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,21 @@
+﻿using log4net.Config;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FarmerJob.Crawler.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlConfigurator.Configure();
+            JobCrawler crawler = new JobCrawler();
+            crawler.Crawl(@"https://cebu.mynimo.com/jobs");
+
+            System.Console.ReadLine();
+        }
+    }
+}
