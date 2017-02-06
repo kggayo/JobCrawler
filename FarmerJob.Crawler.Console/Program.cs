cfd04900b4f38f6a3e0f2e315ea8fead0ef0da98
kgayo <kagaw3000@live.com>
2017-02-06 21:03:40 +0800

    parse html document mynimo

---
+++ FarmerJob.Crawler.Console/Program.cs
@@ -0,0 +1,40 @@
+﻿using HtmlAgilityPack;
+using log4net.Config;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FarmerJob.Crawler.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //XmlConfigurator.Configure();
+            //JobCrawler crawler = new JobCrawler();
+            //crawler.Crawl(@"https://cebu.mynimo.com/jobs/browse/it");
+
+            HtmlDocument doc = new HtmlDocument();
+            doc.Load(@"html\index.html");
+
+            string sourceUrl = "https://cebu.mynimo.com/jobs/browse/it";
+            string jobUrl = "test.html";
+
+            string positionTitle = doc.DocumentNode.SelectSingleNode("//h1[@class='search_highlight']").InnerText.Trim();
+            string location = doc.DocumentNode.SelectSingleNode("//div[@class='location search_highlight']").InnerText.Trim();
+            string companyName = doc.DocumentNode.SelectSingleNode("//span[@class='search_highlight']//a").InnerText.Trim();
+            string postedDate = doc.DocumentNode.SelectSingleNode("//div[@id='contentHeading']//div[@class='meta']").InnerText.Trim().Split(new char[] { '\n' })[0].Replace("Posted on : ","");
+            string jobDescription = doc.DocumentNode.SelectSingleNode("//meta[@name='description']").Attributes["content"].Value;
+            System.Console.ReadLine();
+
+
+        }
+
+        private static void PostJob(string sourceUrl, string jobUrl, string positionTitle, string location, string companyName, string jobPostedDate, string jobDescription, string yrsOfExperience)
+        {
+            
+        }
+    }
+}
--- FarmerJob.Crawler/JobCrawler.cs
+++ FarmerJob.Crawler/JobCrawler.cs
@@ -13,6 +13,9 @@ namespace FarmerJob.Crawler
     public class JobCrawler
     {
         PoliteWebCrawler crawler;
+        private static readonly log4net.ILog log =
+           log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
         public JobCrawler()
         {
             crawler = new PoliteWebCrawler();
@@ -20,50 +23,77 @@ namespace FarmerJob.Crawler
             crawler.PageCrawlCompletedAsync += crawler_ProcessPageCrawlCompleted;
             crawler.PageCrawlDisallowedAsync += crawler_PageCrawlDisallowed;
             crawler.PageLinksCrawlDisallowedAsync += crawler_PageLinksCrawlDisallowed;
+
+            crawler.ShouldCrawlPage((pageToCrawl, crawlContent) => {
+                CrawlDecision decision = new CrawlDecision { Allow = true };
+
+                if (pageToCrawl.Uri.AbsoluteUri.Contains("jobs/browse/it/?levels") || pageToCrawl.Uri.AbsoluteUri.Contains("jobs/browse/it?city") || pageToCrawl.Uri.AbsoluteUri.Contains("jobs/browse/it_"))
+                    return new CrawlDecision { Allow = false, Reason = string.Format("Dont want to crawl {0} pages", pageToCrawl.Uri.AbsoluteUri) };
+
+                if (!pageToCrawl.Uri.AbsoluteUri.Contains("jobs/view") && !pageToCrawl.Uri.AbsoluteUri.Contains("/jobs/browse/it"))
+                    return new CrawlDecision { Allow = false, Reason = string.Format("Dont want to crawl {0} pages", pageToCrawl.Uri.AbsoluteUri) };
+
+                return decision;
+            });
+
+            crawler.ShouldCrawlPageLinks((crawledPage, crawlContext) =>
+            {
+                CrawlDecision decision = new CrawlDecision { Allow = true };
+                
+                if (!crawledPage.Uri.AbsoluteUri.Contains("jobs/browse/it"))
+                    return new CrawlDecision { Allow = false, Reason = "Dont want to crawl links in pages: " + crawledPage.Uri.AbsoluteUri };
+
+                return decision;
+            });
         }
 
         private void crawler_PageLinksCrawlDisallowed(object sender, PageLinksCrawlDisallowedArgs e)
         {
             CrawledPage crawledPage = e.CrawledPage;
-            Console.WriteLine("Did not crawl the links on page {0} due to {1}", crawledPage.Uri.AbsoluteUri, e.DisallowedReason);
+            var result = string.Format("Did not crawl the links on page {0} due to {1}", crawledPage.Uri.AbsoluteUri, e.DisallowedReason);
+            log.Info(result);
         }
 
         private void crawler_PageCrawlDisallowed(object sender, PageCrawlDisallowedArgs e)
         {
             PageToCrawl pageToCrawl = e.PageToCrawl;
-            Console.WriteLine("Did not crawl page {0} due to {1}", pageToCrawl.Uri.AbsoluteUri, e.DisallowedReason);
+            var result = string.Format("Did not crawl page {0} due to {1}", pageToCrawl.Uri.AbsoluteUri, e.DisallowedReason);
+            log.Info(result);
         }
 
         private void crawler_ProcessPageCrawlCompleted(object sender, PageCrawlCompletedArgs e)
         {
             CrawledPage crawledPage = e.CrawledPage;
-
+            string result = "";
             if (crawledPage.WebException != null || crawledPage.HttpWebResponse.StatusCode != HttpStatusCode.OK)
-                Console.WriteLine("Crawl of page failed {0}", crawledPage.Uri.AbsoluteUri);
+                result = string.Format("Crawl of page failed {0}", crawledPage.Uri.AbsoluteUri);
             else
-                Console.WriteLine("Crawl of page succeeded {0}", crawledPage.Uri.AbsoluteUri);
+                result = string.Format("Crawl of page succeeded {0}", crawledPage.Uri.AbsoluteUri);
 
             if (string.IsNullOrEmpty(crawledPage.Content.Text))
-                Console.WriteLine("Page had no content {0}", crawledPage.Uri.AbsoluteUri);
-
+                result = string.Format("Page had no content {0}", crawledPage.Uri.AbsoluteUri);
+            log.Info(result);
             var htmlAgilityPackDocument = crawledPage.HtmlDocument; //Html Agility Pack parser
-            var angleSharpHtmlDocument = crawledPage.AngleSharpHtmlDocument; //AngleSharp parser
+            
+            //var angleSharpHtmlDocument = crawledPage.AngleSharpHtmlDocument; //AngleSharp parser
         }
 
         private void crawler_ProcessPageCrawlStarting(object sender, PageCrawlStartingArgs e)
         {
             PageToCrawl pageToCrawl = e.PageToCrawl;
-            Console.WriteLine("About to crawl link {0} which was found on page {1}", pageToCrawl.Uri.AbsoluteUri, pageToCrawl.ParentUri.AbsoluteUri);
+            var result = string.Format("About to crawl link {0} which was found on page {1}", pageToCrawl.Uri.AbsoluteUri, pageToCrawl.ParentUri.AbsoluteUri);
         }
 
         public void Crawl(string uri)
         {
             CrawlResult result = crawler.Crawl(new Uri(uri));
-
+            string msg = "";
             if (result.ErrorOccurred)
-                Console.WriteLine("Crawl of {0} completed with error: {1}", result.RootUri.AbsoluteUri, result.ErrorException.Message);
+                msg = string.Format("Crawl of {0} completed with error: {1}", result.RootUri.AbsoluteUri, result.ErrorException.Message);
             else
-                Console.WriteLine("Crawl of {0} completed without error.", result.RootUri.AbsoluteUri);
+                msg = string.Format("Crawl of {0} completed without error.", result.RootUri.AbsoluteUri);
+
+            log.Info(msg);
         }
     }
 }
